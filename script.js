de9e01ce46c1e6fc6c8362ee2da02e02337e5c0b
Bahman Dorand <bahman927@gmail.com>
2024-01-08 01:07:22 -0500

    version 2

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-
-
+var lon = 0
+var lat = 0
 document.getElementById('getWeatherBtn').addEventListener('click', function() {
   const city = document.getElementById('cityInput').value;
+  if (city !== " " && city !== ""){
   fetchWeather(city);
+  }
 });
 
 function fetchWeather(city) {
+  
  
-   const apiKey = 'c88c041873c88fa8bc608b33404b1a02'; 
-   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    
+   var apiKey = 'c88c041873c88fa8bc608b33404b1a02'; 
+   var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   
   fetch(apiUrl)
-   .then(response => response.json())
-   .then(data => {
-      displayWeather(data);
-      console.log(data)
+  .then((response) => {
+     if (!response.ok) throw new Error(response.statusText);
+      return response.json()
   })
-  .catch(error => {
-      console.error('Error fetching weather:', error);
+     
+  .then(data => { 
+     displayWeather(data); 
+  })
+  .catch(error => {    
+     var error = 'Error fetching city weather, or no such city exist'
+      displayError(error)
+    
   });
 }
 
 function displayWeather(data) {
+  lat = data.coord.lat
+  lon = data.coord.lon
+  var d_name = data.name +', '+ data.sys.country;
+  
+  var d_name = data.name
+  var d_name = data.name
+  var d_name = data.name
   var weatherInfoDiv = document.getElementById('weatherInfo');
   weatherInfoDiv.innerHTML = `
       <h2 class="text-xl mb-2">${data.name}, ${data.sys.country}</h2>
@@ -31,4 +46,48 @@ function displayWeather(data) {
       <p class="mb-1">Temp low: ${data.main.temp_min}%</p>
       <p class="mb-1">temp high: ${data.main.temp_max}%</p>
   `;
+  
+//   var map = L.map('map').setView([lat, lon], 13);
+
+//   // Add OpenStreetMap layer
+//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//   }).addTo(map);
+  
+//  // Add a marker to the map
+//   L.marker([lat, lon]).addTo(map)
+//     //  .bindPopup(' Here')
+//       .openPopup();
 }
+
+// Function to display error modal
+function displayError(errorMessage) {
+  
+  var modal = document.getElementById('errorModal');
+  var modalContent = document.getElementById('errorMessage');
+
+  // Set error message
+  modalContent.textContent = errorMessage;
+
+  // Show modal
+  modal.classList.remove('hidden');
+  
+}
+
+// Function to close error modal
+document.getElementById('closeModalBtn').addEventListener('click', function() {
+
+  var modal = document.getElementById('errorModal');
+  var cityIN = document.getElementById('cityInput')
+  var weatherInfoDiv = document.getElementById('weatherInfo');
+  weatherInfoDiv.innerHTML = " "
+  modal.classList.add('hidden');
+  cityIN.value = ' '
+  cityIN.placeholder = 'Enter city name'
+  console.log(city)
+
+});
+
+// Initialize the map
+
+
